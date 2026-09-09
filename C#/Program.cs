74a5d541f483686a494f782fd1d01813f082b182
Detskanokgaa <maha816@student.sdu.dk>
2026-09-09 15:52:26 +0200

    5.15

--- C#/Program.cs
+++ C#/Program.cs
@@ -42,3 +42,39 @@ ulong reaminingdays = days % 365;
 Console.WriteLine(reaminingdays);
 // 3 år og 62 dage
 
+double monday = 21.5;
+double tuesday = 23.7;
+double wednesday = 19.6;
+double thursday = 22.5;
+double friday = 25.3;
+double saturday = 21.7;
+double sunday = 18.9;
+Console.WriteLine(tuesday - monday);
+Console.WriteLine(wednesday - tuesday);
+Console.WriteLine(thursday - wednesday);
+Console.WriteLine(friday - thursday);
+Console.WriteLine(saturday - friday);
+Console.WriteLine(sunday - saturday);
+
+
+int ada_lovelace = 36; // https://en.wikipedia.org/wiki/Ada_Lovelace
+int dennis_ritchie = 70; // https://en.wikipedia.org/wiki/Dennis_Ritchie
+int grace_hopper = 85; // https://en.wikipedia.org/wiki/Grace_Hopper
+int hedy_lamarr = 85; // https://en.wikipedia.org/wiki/Hedy_Lamarr
+int edsger_dijkstra = 72; // https://en.wikipedia.org/wiki/Edsger_W._Dijkstra
+int douglas_engelbart = 88; // https://en.wikipedia.org/wiki/Douglas_Engelbart
+float male_avg = (float)(dennis_ritchie + edsger_dijkstra + douglas_engelbart) / 3;
+float female_avg = (float)(ada_lovelace + grace_hopper + hedy_lamarr) / 3;
+float avg = (male_avg + female_avg) / 2;
+float diff = male_avg- female_avg;
+Console.Write("Average lifespan of a male computer scientist: ");
+Console.WriteLine(male_avg);
+Console.Write("Average lifespan of a female computer scientist: ");
+Console.WriteLine(female_avg);
+Console.Write("Average lifespan of a computer scientist: ");
+Console.WriteLine(avg);
+Console.Write("Males live this much longer than females: ");
+Console.WriteLine(diff);
+
+//
+
