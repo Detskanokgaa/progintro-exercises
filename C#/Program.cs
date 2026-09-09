7a8cd8dfae3da2230cffe3111712493a9f035982
Detskanokgaa <maha816@student.sdu.dk>
2026-09-09 16:02:16 +0200

    Opgaverne

--- C#/Program.cs
+++ C#/Program.cs
@@ -78,3 +78,10 @@ Console.WriteLine(diff);
 
 //
 
+int i = 42;
+long l = 56;
+float f = 3.14159F;
+double d = 3.14159 * 10;
+Console.WriteLine("i = {0} \nl = {1,4} \nf = {2} \nd = {3,6:0.00}", i, l, f, d);
+Console.WriteLine("i = {0} \nl = {1,4} \nf = {2} \nd = {3,6:0.00}", d, l, f, i);
+
