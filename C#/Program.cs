5d2cf4142e0a68a864a577d1360f254833633754
Detskanokgaa <maha816@student.sdu.dk>
2026-09-09 14:01:16 +0200

    Program

---
+++ C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        byte max = byte.MaxValue;   // 255
+        Console.WriteLine("Max value: " + max);
+
+        byte overflow = (byte)(max + 1);
+        Console.WriteLine("After adding 1: " + overflow);
+    }
+}
